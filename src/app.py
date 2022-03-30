11a3f1e55ade4cd9b7bdeb69595a341dccafccbf
diegoovmoura <84737681+diegoovmoura@users.noreply.github.com>
2022-03-30 08:38:11 -0300

    criando o flask

---
+++ src/app.py
@@ -0,0 +1,12 @@
+import os
+import sys
+# import sqlalchemy
+# import sqlalchemy.orm
+import flask 
+
+app = flask.Flask(__name__)
+app.secret_key = "oerifjw;DFjniwrnvKesj_slfiun*&"
+
+@app.route("/")
+def index():
+    return flask.send_file("../static/index.html")
