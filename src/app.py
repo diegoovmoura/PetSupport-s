f93c5af226df42e766ca421f3386ffe18bfcccdf
diegoovmoura <84737681+diegoovmoura@users.noreply.github.com>
2022-04-29 16:59:15 -0300

    images working

--- src/app.py
+++ src/app.py
@@ -11,6 +11,6 @@ app.secret_key = "oerifjw;DFjniwrnvKesj_slfiun*&"
 def index():
     return flask.send_file("../static/index.html")
     
-@app.route("/<static_filename>")
+@app.route("/<path:static_filename>")
 def static_files(static_filename):
-    return flask.send_file(f"static/{static_filename}")    
+    return flask.send_file(f"../static/{static_filename}")    
