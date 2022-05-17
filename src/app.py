32796927ec861f33ed84b6fe8d8067f722f9c6a7
diegoovmoura <84737681+diegoovmoura@users.noreply.github.com>
2022-05-17 20:22:09 -0300

    frontend adjustments and intera interaction with backend

--- src/app.py
+++ src/app.py
@@ -3,16 +3,63 @@ import sys
 # import sqlalchemy
 # import sqlalchemy.orm 
 import flask 
-import pytest
 
 app = flask.Flask(__name__)
 app.secret_key = "oerifjw;DFjniwrnvKesj_slfiun*&"
 
 @app.route("/")
-@pytest.fixture()
 def index():
     return flask.send_file("../static/index.html")
     
 @app.route("/<path:static_filename>")
 def static_files(static_filename):
-    return flask.send_file(f"../static/{static_filename}")    
+    return flask.send_file(f"../static/{static_filename}")   
+
+@app.route('/animais', methods = ['GET'])
+def animais():
+    dicionario = [
+        {
+            "descricao": "Gato"
+        },
+        {
+            "descricao": "Cachorro"
+        },
+        {
+            "descricao": "flavio"
+        },
+        {
+            "descricao": "Gato"
+        },
+        {
+            "descricao": "Cachorro"
+        },
+        {
+            "descricao": "flavio"
+        },
+        {
+            "descricao": "Gato"
+        }
+    ]
+    return flask.jsonify(dicionario)
+
+@app.route('/autenticar', methods = ['POST'])
+def autenticar():
+    obj = flask.request.get_json()
+    email = obj['email']
+    senha = obj['senha']
+    emails_validos = [
+        'dovm@cesar.school',
+        'elgr@cesar.school',
+        'ffcl@cesar.school',
+        'jhcc2@cesar.school',
+        'pevs@cesar.school',
+        'csgg@cesar.school',
+        'mk2@cesar.school',
+        'mjwps@cesar.school'
+    ]
+    email_invalido = email not in emails_validos
+    senha_invalida = senha != '12345'
+    if email_invalido or senha_invalida:
+        return flask.jsonify(False)
+    return flask.jsonify(True) 
+
--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-var idTelaAtual = '';
+var idTelaAtual = "telaInicial";
 
 function expande(idElemento, dicionario) {
     var elemento = document.getElementById(idElemento);
@@ -13,35 +13,52 @@ function expande(idElemento, dicionario) {
     novoElemento.removeAttribute("id");
     elementoPai.appendChild(novoElemento);
 }
-function mostraPets() {
+
+function request(args) {
+    var url        = args.url;
+    var method     = args.method;
+    var data       = args.data;
+    var fn         = args.fn;
+    if (method == undefined) {
+        method = "GET";
+    }
+    var body = null;
+    var headers = new Headers()
+    if (data != undefined) {
+        body = JSON.stringify(data);
+        headers.append("Content-Type", "application/json");
+    }
+    console.log(data);
+    fetch(url, {
+        method,
+        body,
+        headers
+    }).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        if (fn != undefined) {
+            fn(data);
+        } else {
+            console.log(data);
+        }
+    }).catch(function(error) {
+        console.log(error);
+    });
+};
+
+function mostraPets(dicionario) {
     var elemento = document.getElementById("telaInicial");
     elemento.classList.remove("d-none");
-    var dicionario = [
-        {
-            descricao: "Gato"
-        },
-        {
-            descricao: "Cachorro"
-        },
-        {
-            descricao: "flavio"
-        },
-        {
-            descricao: "Gato"
-        },
-        {
-            descricao: "Cachorro"
-        },
-        {
-            descricao: "flavio"
-        },
-        {
-            descricao: "Gato"
-        }
-    ]
     for (var i in dicionario) {
         expande("templatePet", dicionario[i]);
     }
+}
+
+function inicializa() {
+    request({
+        "url": "/animais",
+        "fn": mostraPets
+    })
 }
 
 function mostrarTelaPrincipal() {
@@ -53,8 +70,9 @@ function mostrarTelaPrincipal() {
 }
 
 function mostrarTelaSecundaria() {
+    idTelaAtual = "telaSecundaria";
     var telaAnterior = document.getElementById("telaInicial");
-    var telaAtual = document.getElementById("telaSecundaria");
+    var telaAtual = document.getElementById(idTelaAtual);
     telaAnterior.classList.add("d-none");
     telaAtual.classList.remove("d-none");
 }
@@ -62,11 +80,53 @@ function mostrarTelaSecundaria() {
 function mostrarTelaLogin() {
     var header = document.getElementById("header");
     var footer = document.getElementById("footer");    
-    var telaAnterior = document.getElementById("telaInicial");
+    var telaAnterior = document.getElementById(idTelaAtual);
     var telaAtual = document.getElementById("telaTerciaria");
     header.classList.add("d-none");
     footer.classList.add("d-none");
     telaAnterior.classList.add("d-none");
     telaAtual.classList.remove("d-none");
 
+    idTelaAtual = "telaTerciaria";
 }
+
+function autenticar() {
+    var email = document.getElementById("exampleFormControlInput1").value;
+    var senha = document.getElementById("exampleFormControlInput2").value;
+    var result = request({
+        "url": "/autenticar",
+        "method": "POST",
+        "data": {
+            "email": email,
+            "senha": senha
+        },
+        "fn": mostraUsuario
+    })
+}
+
+function mostraUsuario(autenticado) {
+    if (autenticado == true) {
+        var botoes = document.getElementById("botoesLogar")
+        var imgUsuario = document.getElementById("fotoUsuario");
+        botoes.classList.add("d-none");
+        imgUsuario.classList.remove("d-none");
+    }
+    voltarTelaPrincipal();
+}
+
+function voltarTelaPrincipal() {
+    var header = document.getElementById("header");
+    var footer = document.getElementById("footer");    
+    var telaAnterior = document.getElementById(idTelaAtual);
+    var telaAtual = document.getElementById("telaInicial");
+    header.classList.remove("d-none");
+    footer.classList.remove("d-none");
+    telaAnterior.classList.add("d-none");
+    telaAtual.classList.remove("d-none");
+
+    idTelaAtual = "telaInicial";
+}
+
+// function mostrarTelaCadastro(){
+//     var 
+// }
